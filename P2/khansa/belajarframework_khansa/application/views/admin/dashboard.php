cc1ccd1a5874cec9cafed2ab6bef0efb69cebe87
Roki Prasetyo Adi <rokiprasetyoadi@gmail.com>
2019-03-19 16:00:31 +0700

    belajar
    belajar

--- P2/khansa/belajarframework_khansa/application/views/admin/dashboard.php
+++ P2/khansa/belajarframework_khansa/application/views/admin/dashboard.php
@@ -12,16 +12,16 @@
 
   <title>SB Admin - Dashboard</title>
 
-  <link href="<? php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" >
+  <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" >
 
     <!-- Custom fonts for this template-->
-    <link href="<? php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
 
 <!-- Page level plugin CSS-->
-<link href="<? php echo base_url('assets/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
+<link href="<?php echo base_url('assets/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
 
 <!-- Custom styles for this template-->
-<link href="<? php echo base_url('css/sb-admin.css') ?>" rel="stylesheet">
+<link href="<?php echo base_url('css/sb-admin.css') ?>" rel="stylesheet">
 
 </head>
 
