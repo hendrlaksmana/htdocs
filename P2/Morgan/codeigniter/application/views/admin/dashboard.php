84a8164c524d2cfe0aeacd9362d6b0f1475db013
Morgan Ardianto <45646621+morganteng98@users.noreply.github.com>
2019-03-24 14:36:43 +0700

    pemisahan
    lanjutan

--- P2/Morgan/codeigniter/application/views/admin/dashboard.php
+++ P2/Morgan/codeigniter/application/views/admin/dashboard.php
@@ -1,74 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+  <?php include'head.php'; ?><!-- head -->
 
-<?php include'head.php'; ?>
+  <body id="page-top">
+     <?php include'navbar.php'; ?>
+      <div id="wrapper">    
+        <?php include'sidebar.php'; ?><!-- Sidebar -->
+          <div id="content-wrapper">
+            <div class="container-fluid">
+              <?php include'breadcrumb.php'; ?><!-- Breadcrumbs-->
+              <?php include'iconcard.php'; ?><!-- Icon Cards-->
+              <?php include'areachart.php'; ?><!-- Area Chart Example-->
+              <?php include'datatable.php'; ?><!-- DataTables Example --> 
+            </div><!-- /.container-fluid -->
+              <?php include'footer.php'; ?><!-- Sticky Footer -->
+          </div><!-- /.content-wrapper -->
+      </div><!-- /#wrapper -->
 
-<body id="page-top">
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
 
-<?php include'navbar.php'; ?>
+    <?php include'logoutmodal.php'; ?>    <!-- Logout Modal-->
+    <?php include'btsdanjs.php'; ?>    <!-- Bootstrap core JavaScript-->
 
-  <div id="wrapper">
-
-    <!-- Sidebar -->
-    <?php include'sidebar.php'; ?>
-
-    <div id="content-wrapper">
-
-      <div class="container-fluid">
-
-        <!-- Breadcrumbs-->
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="#">Dashboard</a>
-          </li>
-          <li class="breadcrumb-item active">Overview</li>
-        </ol>
-
-        <!-- Icon Cards-->
-<?php include'iconcard.php'; ?>
-        
-
-        <!-- Area Chart Example-->
-        <div class="card mb-3">
-          <div class="card-header">
-            <i class="fas fa-chart-area"></i>
-            Area Chart Example</div>
-          <div class="card-body">
-            <canvas id="myAreaChart" width="100%" height="30"></canvas>
-          </div>
-          <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-        </div>
-
-        <!-- DataTables Example -->
-<?php include'datatable.php'; ?>
-        
-      </div>
-      <!-- /.container-fluid -->
-
-      <!-- Sticky Footer -->
-      <footer class="sticky-footer">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright © Your Website 2019</span>
-          </div>
-        </div>
-      </footer>
-
-    </div>
-    <!-- /.content-wrapper -->
-
-  </div>
-  <!-- /#wrapper -->
-
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
-
-  <!-- Logout Modal-->
-<?php include'logoutmodal.php'; ?>
-  <!-- Bootstrap core JavaScript-->
-<?php include'btsdanjs.php'; ?>
-</body>
-
+  </body>
 </html>
