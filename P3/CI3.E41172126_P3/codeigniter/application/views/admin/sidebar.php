f799f5b7230445da1f4eecbf391b3c6e8e5ced1f
morganteng98 <45646621+morganteng98@users.noreply.github.com>
2019-04-05 17:04:38 +0700

    Update sidebar.php

--- P3/CI3.E41172126_P3/codeigniter/application/views/admin/sidebar.php
+++ P3/CI3.E41172126_P3/codeigniter/application/views/admin/sidebar.php
@@ -8,13 +8,13 @@
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="fas fa-fw fa-folder"></i>
-          <span>Pages</span>
+          <span>Data</span>
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <h6 class="dropdown-header">Login Screens:</h6>
-          <a class="dropdown-item" href="login.html">Login</a>
-          <a class="dropdown-item" href="register.html">Register</a>
-          <a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
+          
+          <a class="dropdown-item" href="barang">Data Barang</a>
+          <a class="dropdown-item" href="">Register</a>
+          
           <div class="dropdown-divider"></div>
           <h6 class="dropdown-header">Other Pages:</h6>
           <a class="dropdown-item" href="404.html">404 Page</a>
