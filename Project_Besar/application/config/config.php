e7812aa0eccf2e93897e50c3bd4d8b8d3dd53fa6
Roki Prasetyo Adi <rokiprasetyoadi@gmail.com>
2019-05-12 11:23:13 +0700

    Project besar
    membuat tampilan admin, mengatur controller, mengatur partial design,

--- Project_Besar/application/config/config.php
+++ Project_Besar/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'localhost/Project_Besar';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
