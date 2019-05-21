d1b9605989e89e6064b45eee06f82c6c97680c31
Morgan Ardianto <45646621+morganteng98@users.noreply.github.com>
2019-05-21 15:41:36 +0700

    Partial Template
    Tampilan web member

---
+++ web_sumbermakmur/application/views/member/_include/head.php
@@ -0,0 +1,18 @@
+ <title>Shoppers &mdash; Colorlib e-Commerce Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
+    <link rel="stylesheet" href=<?php echo base_url('assets/member/fonts/icomoon/style.css')?>>
+
+    <link rel="stylesheet" href=<?php echo base_url('assets/member/css/bootstrap.min.css')?>>
+    <link rel="stylesheet" href=<?php echo base_url('assets/member/css/magnific-popup.css')?>>
+    <link rel="stylesheet" href=<?php echo base_url('assets/member/css/jquery-ui.css')?>>
+    <link rel="stylesheet" href=<?php echo base_url('assets/member/css/owl.carousel.min.css')?>>
+    <link rel="stylesheet" href=<?php echo base_url('assets/member/css/owl.theme.default.min.css')?>>
+
+
+    <link rel="stylesheet" href=<?php echo base_url('assets/member/css/aos.css')?>>
+
+    <link rel="stylesheet" href=<?php echo base_url('assets/member/css/style.css')?>>
+    
--- web_sumbermakmur/application/views/member/index.php
+++ web_sumbermakmur/application/views/member/index.php
@@ -1,1 +1,60 @@
-Hallo ini member
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  		 <?php $this->load->view("member/_include/head.php") ?>  
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+    <header class="site-navbar" role="banner">
+      	<?php $this->load->view("member/_include/navbar.php") ?>
+      
+    </header>
+
+    <div class="site-blocks-cover" style="background-image: url(assets/member/images/hero_1.jpg);" data-aos="fade">
+      <div class="container">
+        <div class="row align-items-start align-items-md-center justify-content-end">
+          <div class="col-md-5 text-center text-md-left pt-5 pt-md-0">
+            <h1 class="mb-2">Finding Your Perfect Shoes</h1>
+            <div class="intro-text text-center text-md-left">
+              <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla. </p>
+              <p>
+                <a href="#" class="btn btn-sm btn-primary">Shop Now</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<!--     <div class="site-section site-section-sm site-blocks-1">
+  <div class="container"></div>
+</div>
+
+<div class="site-section site-blocks-2">
+  <div class="container">
+    <div class="row"></div>
+  </div>
+</div>
+
+<div class="site-section block-3 site-blocks-2 bg-light">
+  <div class="container"></div>
+</div>
+
+<div class="site-section block-8">
+  <div class="container"></div>
+</div>
+ -->
+    <footer class="site-footer border-top">
+   	 <?php $this->load->view("member/_include/footer.php") ?>
+      
+
+      
+    </footer>
+  </div>
+
+    
+  <?php $this->load->view("member/_include/js.php") ?>
+
+  </body>
+</html>
