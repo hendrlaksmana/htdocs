8e4ec2663b7730208159623c1b16adbde93b296d
Morgan Ardianto <45646621+morganteng98@users.noreply.github.com>
2019-05-22 16:20:13 +0700

    Partial Template
    Tampilan web member

--- web_sumbermakmur/application/views/member/_include/footer.php
+++ web_sumbermakmur/application/views/member/_include/footer.php
@@ -41,31 +41,22 @@
             <div class="block-5 mb-5">
               <h3 class="footer-heading mb-4">Contact Info</h3>
               <ul class="list-unstyled">
-                <li class="address">203 Fake St. Mountain View, San Francisco, California, USA</li>
+                <li class="address">Jember Sumbersari</li>
                 <li class="phone"><a href="tel://23923929210">+2 392 3929 210</a></li>
-                <li class="email">emailaddress@domain.com</li>
+                <li class="email">sumbermakmur@gmail.com</li>
               </ul>
             </div>
 
-            <div class="block-7">
-              <form action="#" method="post">
-                <label for="email_subscribe" class="footer-heading">Subscribe</label>
-                <div class="form-group">
-                  <input type="text" class="form-control py-4" id="email_subscribe" placeholder="Email">
-                  <input type="submit" class="btn btn-sm btn-primary" value="Send">
-                </div>
-              </form>
-            </div>
+            
           </div>
-        </div>
-        <div class="row pt-5 mt-5 text-center">
-          <div class="col-md-12">
-            <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script data-cfasync="false" src="assets/member/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-          </div>
-          
         </div>
       </div>
+
+        <div class="row pt-5 mt-5 text-center ">    
+          <div class="col-md-12">
+            <h5><b>Powered By :</b></h5>
+            <img src="<?php echo base_url('assets/sumber_makmur/polije.png"')?>" style="height: 100px;width: 100px;">
+            <img src="<?php echo base_url('assets/sumber_makmur/ti.png"')?>" style="height: 120px;width: 120px;"><p>
+            <h6>Copyright Sumber Makmur E-Commerce Project </br> © 2019 Copyright </h6>
+          </div>     
+        </div>
--- web_sumbermakmur/application/views/member/_include/navbar.php
+++ web_sumbermakmur/application/views/member/_include/navbar.php
@@ -1,4 +1,4 @@
-<div class="site-navbar-top">
+<div class="site-navbar-top"  style="background-color: white">
         <div class="container">
           <div class="row align-items-center">
 
@@ -11,7 +11,7 @@
 
             <div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
               <div class="site-logo">
-                <a href="index.html" class="js-logo-clone">Shoppers</a>
+                <h2>SumberMakmur</h2>
               </div>
             </div>
 
@@ -38,34 +38,22 @@
 <nav class="site-navigation text-right text-md-center" role="navigation">
         <div class="container">
           <ul class="site-menu js-clone-nav d-none d-md-block">
-            <li class="has-children active">
-              <a href="index">Home</a>
-              <ul class="dropdown">
-                <li><a href="#">Menu One</a></li>
-                <li><a href="#">Menu Two</a></li>
-                <li><a href="#">Menu Three</a></li>
-                <li class="has-children">
-                  <a href="#">Sub Menu</a>
-                  <ul class="dropdown">
-                    <li><a href="#">Menu One</a></li>
-                    <li><a href="#">Menu Two</a></li>
-                    <li><a href="#">Menu Three</a></li>
-                  </ul>
-                </li>
-              </ul>
+            <li class="active">
+              <a href="<?php echo base_url() ?>">Home</a>
             </li>
             <li class="has-children">
-              <a href="about.html">About</a>
+              <a href="#">Katalog</a>
               <ul class="dropdown">
-                <li><a href="#">Menu One</a></li>
-                <li><a href="#">Menu Two</a></li>
-                <li><a href="#">Menu Three</a></li>
+                <li><a href="#">Alat Pertanian</a></li>
+                <li><a href="#">Benih</a></li>
+                <li><a href="#">Bibit</a></li>
+                <li><a href="#">Pupuk</a></li>
               </ul>
             </li>
-            <li><a href="shop.html">Shop</a></li>
-            <li><a href="#">Catalogue</a></li>
-            <li><a href="#">New Arrivals</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="#">Informasi Seputar Pertanian</a></li>
+            <li><a href="#">Cara Pembayaran</a></li>
+            <li><a href="#">Tentang Toko</a></li>
+            
           </ul>
         </div>
       </nav>
