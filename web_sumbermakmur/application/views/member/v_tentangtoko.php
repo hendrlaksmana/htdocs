26f798d56fbf33cee756b5c72ebd0023c98e224d
khansaidr24 <45646505+khansaidr24@users.noreply.github.com>
2019-07-07 20:17:03 +0700

    Update v_tentangtoko.php

--- web_sumbermakmur/application/views/member/v_tentangtoko.php
+++ web_sumbermakmur/application/views/member/v_tentangtoko.php
@@ -3,7 +3,8 @@
   <head>
     <title> Tentang toko</title>
 
-  		 <?php $this->load->view("member/_include/head.php") ?>  
+  		 <?php $this->load->view("member/_include/head.php") ?> 
+
   </head>
   <body>
   
@@ -19,23 +20,24 @@
  		 </div>
 	</div>
 
-<form align="center">
-    <tr bgcolor="#5974a0">
-      <td height="250" width="700">
-        <img src="<?= base_url('assets/sumber_makmur/sumbermakmur.jpeg'); ?>">
-    </td>
-  </tr>
 
-  <tr>
-    <td align="center" height="30">
-      <h3 style="color: green;"> toko pertanian sumber makmur terletak di jl trunojoyo no 94 - jember (depan toko buku gramedia) menjual alat alat pertanian beserta pupuk pupuk. no tlp : (0331) 484562 - 082230718863 </h3>
-    </td>
-  </tr>
+<div  class="container">
+  <div style="border-radius: 8%" class="card mb-6 mr-2 ml-2"  align="center">
+  <img  class="card-img-top"  height="300px" src="<?= base_url('assets/sumber_makmur/sumbermakmur.jpeg'); ?>" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title font-weight-bold">Toko Pertanian Sumber Makmur</h5>
+    <p  class="card-text ">toko sumber makmur terletak di Jl. Trunojoyo no.94 di depan toko gramedia jember. toko sumber makmur menyediakan pupuk pupuk pertanian juga alat alat penunjang pertanian, seperti cangkul, sabit, dan peralatan lainnya.</br>
+      <br>mengapa harus berbelanja di toko sumber makmur?</br>
+      <br>karena di toko sumber makmur menyediakan berbagai macam pupuk untuk segala macam tanaman dengan harga yang murah. selain harga yang murah, kualitas dari produknya pun terjamin dan bergaransi. selain itu jika berbelaja di sumber makmur bisa dilakukan dari rumah, dengan cara memesan secara online melalui website dan aplikasi andorid bernama "sumber makmur" yang bisa di unduh di playstore. setelah dilakukan pemesanan, maka barang yang di pesan akan diantar oleh kurir kami ke rumah anda dengan aman.</br>
+    </p>
+  </div>
+</div>
+</div>
 
 
   
 
-</form>
+</div>
 
     
  		 </div>
